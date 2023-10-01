8a241b086dc09d33a22032bdb12672c65cf9cc4e
alfa <alfaiv@yandex.ru>
2023-10-01 16:49:48 +0300

    add templating and simple adaptability

--- public/components/filmSelection/filmSelection.mjs
+++ public/components/filmSelection/filmSelection.mjs
@@ -12,10 +12,42 @@ export class FilmSelection {
   }
 
   render() {
-    // Чтобы это работало, нужно импортировать handlebars.runtime.js
-    console.log(this.#parent);
+    const films = {
+      collection: {
+        collectionName: "Боевики",
+        films: {
+          film1: {
+            poster_href: "../../icons/Poster.jpg",
+            name: "film_1",
+            rating: 4.5,
+          },
+          film2: {
+            poster_href: "../../icons/Poster.jpg",
+            name: "film_2",
+            rating: 4.1,
+          },
+          film3: {
+            poster_href: "../../icons/Poster.jpg",
+            name: "film_3",
+            rating: 4.5,
+          },
+          film4: {
+            poster_href: "../../icons/Poster.jpg",
+            name: "film_4",
+            rating: 3,
+          },
+          film5: {
+            poster_href: "../../icons/Poster.jpg",
+            name: "film_5",
+            rating: 2,
+          },
+        },
+      },
+    };
+
+    console.log(films);
+
     const template = Handlebars.templates["filmSelection.hbs"];
-    console.log(template);
-    this.#parent.innerHTML = template();
+    this.#parent.innerHTML = template(films);
   }
 }
