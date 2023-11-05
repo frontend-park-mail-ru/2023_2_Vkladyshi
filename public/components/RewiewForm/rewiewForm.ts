571441238589ecba8d824546c50c671741d38502
alfa <alfaiv@yandex.ru>
2023-11-05 22:30:12 +0300

    add .ts file to rewiewForm

---
+++ public/components/RewiewForm/rewiewForm.ts
@@ -0,0 +1,17 @@
+import { Component } from '@components/component';
+import * as templateRewiewForm from '@components/RewiewForm/rewiewForm.hbs';
+
+/**
+ * Класс рендеринга формы заполенения отзыва
+ * @class RewiewForm
+ * @typedef {RewiewForm}
+ */
+export class RewiewForm extends Component {
+  /**
+   * Метод рендеринга элемента
+   * @return {string} html форма заполенения отзыва
+   */
+  render() {
+    return templateRewiewForm();
+  }
+}
