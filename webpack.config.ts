a4ab14d63c21fc58ad676c5f0abc68574dc973c8
Gokert <aleksej.y.60@mail.ru>
2023-12-25 16:33:36 +0300

    deploy

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,20 +2,49 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
-const isDev = process.env.NODE_ENV === 'development';
+const isDev = process.env.NODE_ENV === 'production';
 const isProd = !isDev;
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: ['./public/index.ts'],
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
   },
+  optimization: {
+    minimizer: [
+      new TerserPlugin(), // Минимизация JavaScript
+      new OptimizeCSSAssetsPlugin({}), // Минимизация CSS
+    ],
+  },
 
   plugins: [
+    new ImageMinimizerPlugin({
+      minimizerOptions: {
+        plugins: [
+          ['gifsicle', { interlaced: true }],
+          ['jpegtran', { progressive: true }],
+          ['optipng', { optimizationLevel: 5 }],
+          [
+            'svgo',
+            {
+              plugins: [
+                {
+                  removeViewBox: false,
+                },
+              ],
+            },
+          ],
+        ],
+      },
+    }),
+
     new HtmlWebpackPlugin({
       template: './public/index.html',
       filename: 'index.html',
